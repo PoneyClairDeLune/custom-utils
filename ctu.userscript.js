ce9a4e2f30a34aa732eeb2390be6609aad1c722c
Lumière Élevé <88174309+PoneyClairDeLune@users.noreply.github.com>
2024-05-20 14:58:46 +0100

    Update ctu.userscript.js

--- ctu.userscript.js
+++ ctu.userscript.js
@@ -3,7 +3,7 @@
 // @namespace   Violentmonkey Scripts
 // @match       https://www.twitch.tv/*
 // @grant       none
-// @version     0.1.6
+// @version     0.1.8
 // @author      Lumière Élevé
 // @description 19/04/2024, 17:33:32; 25/04/2024, 16:05:46
 // ==/UserScript==
@@ -76,7 +76,7 @@ let createHashedArray = (input) => {
 	return hashedArray;
 };
 
-const blockedUsers = createHashedArray("00_aaliyah,00_ava,00_darla,0__sophia,0_lonely_egirl,4jug,8hvdes,8roe,a_ok,anotherttvviewer,asmr_miyu,axfq,captainshadowthehedgehog8,commanderroot,confidence,coochieman6942021,d0nk7,david3cetqd,dn9n,drapsnatt,edward4rijf4,feelssunnyman,framerates,fwoxty,georgew2ms8p,im_socurious,iwill_beback,jackerhikaru,jasonc8l4wl,jasonnvs1x4,jeffl0ab8p,joseph1zj6gg,lady94two,lilfuwafuw,markzynk,mersufy,michaelqmz35a,princessdark666,psh_aa,redterror_,regressz,richard9oipjx,richie_rich_9000,rockn__,rodorigesuuu,sarahaley011,scorpyl2,sukoxi,tarsai,tiggerbandit,vincenine,vlmercy,williamvea2rw,yosharpia,mizoreai,pinkamena_usuario,tiniencdmxtv".split(","));
+const blockedUsers = createHashedArray("00_aaliyah,00_ava,00_darla,0__sophia,0_lonely_egirl,4jug,8hvdes,8roe,a_ok,anotherttvviewer,asmr_miyu,axfq,captainshadowthehedgehog8,commanderroot,confidence,coochieman6942021,d0nk7,david3cetqd,dn9n,drapsnatt,edward4rijf4,feelssunnyman,framerates,fwoxty,georgew2ms8p,im_socurious,iwill_beback,jackerhikaru,jasonc8l4wl,jasonnvs1x4,jeffl0ab8p,joseph1zj6gg,lady94two,lilfuwafuw,markzynk,mersufy,michaelqmz35a,princessdark666,psh_aa,redterror_,regressz,richard9oipjx,richie_rich_9000,rockn__,rodorigesuuu,sarahaley011,scorpyl2,sukoxi,tarsai,tiggerbandit,vincenine,vlmercy,williamvea2rw,yosharpia,littleshyfim,mizoreai,pinkamena_usuario,tiniencdmxtv".split(","));
 const ignoreChatRestrictionKeywords = "connec,verbind,csatlakozás,подключение,接続,連線,mode,modus,mód,modo,moda,режим,モード,模式,follow,suiv,folg,köve,segu,фолловеров,フォロー,追蹤".normalize("NFD").split(",");
 
 let locationHash;
@@ -93,10 +93,16 @@ let selectMinecraftStream = () => {
 		runCount ++;
 	});
 	if (targets.length) {
-		return `${targets[Math.floor(Math.random() * targets.length)]}#minecraftGet`;
+		return {
+			href: `${targets[Math.floor(Math.random() * targets.length)]}#minecraftGet`,
+			stay: Math.min(1, targets * 0.2)
+		};
 	};
 	if (!runCount) {
-		return `wait`;
+		return {
+			href: `wait`,
+			stay: 0
+		};
 	};
 };
 
@@ -135,10 +141,10 @@ let startObserver = (appMount) => {
 			switch (self.location.pathname) {
 				case "/directory/all/tags/pony": {
 					let selected = selectMinecraftStream();
-					if (selected == "wait") {
+					if (selected.href == "wait") {
 						console.debug(`Waiting...`);
-					} else if (selected && Math.random() < 0.8) {
-						location.href = selected;
+					} else if (selected.href && Math.random() < selected.stay) {
+						location.href = selected.href;
 					} else {
 						location.href = "/directory/all/tags/minecraft";
 					};
@@ -146,10 +152,10 @@ let startObserver = (appMount) => {
 				};
 				case "/directory/all/tags/minecraft": {
 					let selected = selectMinecraftStream();
-					if (selected == "wait") {
+					if (selected.href == "wait") {
 						console.debug(`Waiting...`);
-					} else if (selected) {
-						location.href = selected;
+					} else if (selected.href) {
+						location.href = selected.href;
 					} else {
 						location.href = "/directory/all/tags/minecraft";
 					};
@@ -188,7 +194,7 @@ let startObserver = (appMount) => {
 									};
 									if (chatRestriction.indexOf(ignoreChatRestrictionKeywords[i]) > -1) {
 										restrictionRespected = false;
-										//console.debug(`[Custom Twitch Utils] "${ignoreChatRestrictionKeywords[i]}" found in "${chatRestriction}".`);
+                    //console.debug(`[Custom Twitch Utils] "${ignoreChatRestrictionKeywords[i]}" found in "${chatRestriction}".`);
 									} else {
 										//console.debug(`[Custom Twitch Utils] "${ignoreChatRestrictionKeywords[i]}" not found in "${chatRestriction}".`);
 									};
