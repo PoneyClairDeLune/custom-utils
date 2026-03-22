20f421e511567f531c8a75b0d38bb1f0c711a64e
Lumière Élevé <88174309+PoneyClairDeLune@users.noreply.github.com>
2026-03-22 19:49:25 +0000

    Refactor pattingJob function and adjust interval

--- twitch/patTheAdoraball.js
+++ twitch/patTheAdoraball.js
@@ -1,28 +1,35 @@
 "use strict";
 
-let redeemName = "Patpat the pega";
+self.redeemName = "Patpat the pega";
 
-let pattingJob = () => {
-	if ($("div#channel-points-reward-center-body")) {
-		if ($("div.rewards-list")) {
-			// Try to proceed to the redeem
-			$(`div.reward-list-item>div:has(p[title='${redeemName}']) button`)?.click();
-		} else {
-			// Try to redeem
-			let redeemButton = $("div#channel-points-reward-center-body button");
-			if (redeemButton && !redeemButton?.disabled) {
-				// Redeem with enough points
-				redeemButton.click();
-			} else {
-				// Close the redeem for now
-				let redeemHeader = $("div.rewards-popover-header > div");
-				redeemHeader.children[redeemHeader.children.length - 1]?.querySelector("button")?.click();
-			};
-		};
-	} else {
-		// Try to open the rewards panel
-		$("button:has(.channel-points-icon)")?.click();
-	};
+let pattingJob = function () {
+    if ($("div#channel-points-reward-center-body")) {
+        if ($("div.rewards-list")) {
+            // Try to proceed to the redeem
+            let rE = $(`div.rewards-list>div>div:has(img[alt='${redeemName}']) button`);
+            console.debug(`Selection phase: %o`, rE);
+            rE?.click();
+            return 1;
+        } else {
+            // Try to redeem
+            console.debug(`Redeem phase.`);
+            let redeemButton = $("div#channel-points-reward-center-body button");
+            if (redeemButton && !redeemButton?.disabled) {
+                // Redeem with enough points
+                redeemButton.click();
+            } else {
+                // Close the redeem for now
+                let redeemHeader = $("div.rewards-popover-header > div");
+                redeemHeader.children[redeemHeader.children.length - 1]?.querySelector("button")?.click();
+            };
+            return 2;
+        };
+    } else {
+        console.debug(`Idle phase.`);
+        // Try to open the rewards panel
+        $("button:has(.channel-points-icon)")?.click();
+        return 0;
+    };
 };
 
-self.pattingThread = setInterval(pattingJob, 4000);
+self.pattingThread = setInterval(pattingJob, 5000);
